9d68e1be49aa0985f2eb780e0a8931b96ee08416
ghuang123 <gracehuang133@gmail.com>
2017-04-22 14:07:51 -0400

    added partials

--- index.js
+++ index.js
@@ -9,14 +9,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
 // This file is what handles incoming requests and
 // serves files to the browser, or executes server-side code
